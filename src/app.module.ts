7eba81a8e7da92717fd51923e167ae6b850efe42
David Ordoñez <david.ordonez@live.com.mx>
2022-09-08 02:30:11 -0500

    schemas

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductosModule } from './productos/productos.module';
 import { MensajesModule } from './mensajes/mensajes.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ProductosModule, MensajesModule],
+  imports: [ProductosModule, MensajesModule, MongooseModule.forRoot('mongodb://localhost/ecommerce')],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/schema/producto.schema.ts
@@ -0,0 +1,16 @@
+import { Prop,Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type ProductosDocument = Productos & Document;
+
+@Schema()
+export class Productos {
+    @Prop()
+    nombre: string;
+    
+    @Prop()
+    precio: number;
+    
+    @Prop()
+    foto: string;
+}
